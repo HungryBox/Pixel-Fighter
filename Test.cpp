8a38ab55ee67c43d310311ad6e9e2f5840cd7be3
George Gosieski <george.js.gosieski@gmail.com>
2015-03-16 20:34:50 -0700

    Quick test of git using iterm interface

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,18 @@
 #include "iostream"
 
-int main(){
-  std::cout << "Hello World!";
+int multiply(double a, double b)
+{
+  double r;
+  r = a * b;
+  return r;
+}
+
+int main()
+{
+  std::cout << "Hello World!\n";
+  std::cout << "Why is compiling this so difficult?!\n";
+  std::cout << "I can multiply!\n";
+  std::cout << "2 * 3 = " << multiply(2,3) << "\n";
   return 0;
 }
+
